a3f320bc2698074fcdf2e8bda253f4810a12bdd8
zjxmolls <zjxmolls@outlook.com>
2025-11-29 04:35:03 +0800

    modified .gitignore; export templates to the package

--- YR_MPE/__init__.py
+++ YR_MPE/__init__.py
@@ -1,2 +1,4 @@
-# Poly demo package marker
+from .templates.base_plugin_ui import BasePlugin
+from .templates.base_process_thread import BaseProcessThread
 
+__all__ = ['BasePlugin', 'BaseProcessThread']
